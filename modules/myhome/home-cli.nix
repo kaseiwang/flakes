215717993637a1cda64159bcd67d0f126a6dfd9e
Kasei Wang <kasei@kasei.im>
2024-11-01 16:55:39 +0800

    cleanup workaround

--- modules/myhome/home-cli.nix
+++ modules/myhome/home-cli.nix
@@ -80,8 +80,5 @@
     };
   };
 
-  # workaround https://github.com/NixOS/nixpkgs/issues/196651
-  manual.manpages.enable = false;
-
   home.stateVersion = "22.05";
 }
--- nixos/n3160/home.nix
+++ nixos/n3160/home.nix
@@ -1,7 +1,4 @@
 { pkgs, lib, config, ... }:
 {
-  # workaround https://github.com/NixOS/nixpkgs/issues/196651
-  manual.manpages.enable = false;
-
   home.stateVersion = "22.05";
 }
