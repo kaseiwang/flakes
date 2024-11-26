92a3c75b7c57a97f1de422f6bf50209f5d738164
Kasei Wang <kasei@kasei.im>
2024-11-26 14:20:09 +0800

    update networkmanager config

--- nixos/kasei-c940/networking.nix
+++ nixos/kasei-c940/networking.nix
@@ -138,6 +138,7 @@ with pkgs.lib;
           "NetEase-5G" = {
             connection = {
               id = "NetEase-5G";
+              stable-id = ''''${CONNECTION}-''${BOOT}-''${DEVICE}'';
               type = "wifi";
             };
             wifi = {
@@ -158,7 +159,7 @@ with pkgs.lib;
               dns = "127.0.0.1";
               ignore-auto-dns = "true";
               method = "auto";
-              dhcp-client-id = "mac";
+              dhcp-client-id = "stable";
               dhcp-send-hostname = "false";
               dhcp-vendor-class-identifier = "android-dhcp-14";
             };
