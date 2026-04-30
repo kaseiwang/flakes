3c0d0ef417654024635554db171270486fc3e705
Kasei Wang <kasei@kasei.im>
2026-05-01 01:02:12 +0800

    add cachix substituters

--- nixos/kasei-c940/configuration.nix
+++ nixos/kasei-c940/configuration.nix
@@ -56,6 +56,12 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://cache.nixos.org/"
       ];
+      extra-substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 
